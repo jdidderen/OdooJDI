3f138964ed823f34849b2f94a36924e305824f22
Didderen Jérémy <didderen.jeremy@jdi.me>
2019-07-07 19:01:10 +0200

    belfius import - try decode

--- belfius_import/models/belfius_import.py
+++ belfius_import/models/belfius_import.py
@@ -133,7 +133,7 @@ class BelfiusImportLine(models.Model):
             partner_vals['zip'] = zip[0]
             partner_vals['city'] = city[0]
         if data.get('partner_account', ''):
-            bank_vals['acc_number'] = data.get('partner_account').encode(encoding='utf-8', errors='strict')
+            bank_vals['acc_number'] = data.get('partner_account').decode('utf-8')
         if data.get('partner_country', ''):
             country = res_country.search([('code', '=', data.get('partner_country'))], limit=1)
             if country:
