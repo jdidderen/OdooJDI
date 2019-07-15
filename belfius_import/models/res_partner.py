2d703c2d7387558d611c4ae042d3921db8a49a07
Didderen Jérémy <didderen.jeremy@jdi.me>
2019-07-15 18:47:10 +0200

    belfius import - other names

--- belfius_import/models/res_partner.py
+++ belfius_import/models/res_partner.py
@@ -7,6 +7,6 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    other_name_ids = fields.Many2many(comodel_name="other.name",)
+    other_name_ids = fields.Many2many(relation="other.name",)
 
 
