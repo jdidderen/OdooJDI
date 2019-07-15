2363c7ad2b3b73a05b7a57f2762fd3c986420bad
Didderen Jérémy <didderen.jeremy@jdi.me>
2019-07-15 18:48:01 +0200

    belfius import - other names

--- belfius_import/models/other_name.py
+++ belfius_import/models/other_name.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models, tools, _
 _logger = logging.getLogger(__name__)
 
 class OtherName(models.Model):
-    _inherit = "other.name"
+    _name = "other.name"
 
     name = fields.Char(required=True,)
 
