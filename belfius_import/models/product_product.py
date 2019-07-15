f7ff368f213168c2acd39407fccfe9540a12b49e
Didderen Jérémy <didderen.jeremy@jdi.me>
2019-07-15 18:54:09 +0200

    belfius import - other names

--- belfius_import/models/product_product.py
+++ belfius_import/models/product_product.py
@@ -7,5 +7,5 @@ _logger = logging.getLogger(__name__)
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    other_name_ids = fields.Many2many(relation="other.name",)
+    other_name_ids = fields.Many2many(comodel_name="other.name",)
 
